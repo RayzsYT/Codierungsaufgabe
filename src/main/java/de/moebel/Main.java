c49a44aee2d8ce21956500dcfa159ef27e1cb05b
Alihan <51991564+RayzsYT@users.noreply.github.com>
2024-07-26 20:39:32 +0200

    Change & Add Comments, Fix Future Issue

--- src/main/java/de/moebel/Main.java
+++ src/main/java/de/moebel/Main.java
@@ -3,7 +3,19 @@ package de.moebel;
 import de.moebel.utils.*;
 import java.util.Scanner;
 
+/**
+ * This class is the main-class and also the first class the jar interact with at the very beginning.
+ * In here it's decided whether the method should be executed only once, or repeatedly.
+ * @author Alihan
+ */
+
 public class Main {
+
+    /**
+     * This is the first method being called when the jar has been executed.
+     *
+     * @param args The used arguments when the jar has been executed.
+     */
 
     public static void main(String[] args) {
         final int length = args.length -1;
@@ -30,13 +42,26 @@ public class Main {
         System.out.print("> ");
 
         Scanner scanner = new Scanner(System.in);
+        String line;
+
+        // Checks if someone has pressed enter
         while(scanner.hasNext()) {
-            printResult(timer, scanner.nextLine());
+            line = scanner.nextLine();
+
+            // Do nothing and repeat the loop when the line is empty or blank to avoid future issues.
+            if(line.isBlank() || line.isEmpty()) continue;
+
+            printResult(timer, line);
             System.out.print("> ");
         }
 
     }
 
+    /**
+     *
+     * @param timer From the Timer-class to calculate how long the process took.
+     * @param text The input text to get the accumulated value from.
+     */
     private static void printResult(Timer timer, String text) {
         System.out.println("Input: " + text);
 
